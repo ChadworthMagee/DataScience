bcffc63a6333bd2ee3ff0b7ca2741eba0122b4cb
Wijnand <wijnand@localhost.localdomain>
2014-04-16 13:16:42 +0200

    week_3 completed assignments

--- Week_3/best.R
+++ Week_3/best.R
@@ -6,24 +6,28 @@ best <- function(state, outcome)
 
         if(outcome == "heart attack")
         {
-                test <- filter_outcome[order(filter_outcome$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack) , ]
+                test <- filter_outcome[order(as.numeric(as.character(filter_outcome$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack))) , ]
         }
         else if (outcome == "heart failure")
         {
-                test <- filter_outcome[order(filter_outcome$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure) , ] 
+                test <- filter_outcome[order(as.numeric(as.character(filter_outcome$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure))) , ] 
         }
         else if (outcome == "pneumonia")
         {
-                #print(class(filter_outcome$Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia))
                 test <- filter_outcome[order(as.numeric(as.character(filter_outcome$Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia))) , ]
-                min <- min(as.numeric(as.character(filter_outcome$Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia)))
         }
-                
-        hospital <- test$Hospital.Name[1]
         #View(test)
-        print(hospital)
-        #print(min)
-        
+        if(nrow(test) == 0)
+        {
+                stop("invalid state")
+        }
+        else
+        {
+                hospital <- as.character(test$Hospital.Name[1])
+                print(hospital)
+                View(test)
+                hospital
+        }
         ## Check that state and outcome are valid
         ## Return hospital name in that state with lowest 30-day death
         ## rate
