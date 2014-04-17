ac3049090e3b91c51147691e02d757a9425f3402
Wijnand <wijnand@localhost.localdomain>
2014-04-17 13:27:43 +0200

    solution for columns names

--- Week_3/rankhospital.R
+++ Week_3/rankhospital.R
@@ -11,20 +11,58 @@ rankhospital <- function(state, outcome, num = "best")
         
         if(outcome == "heart attack")
         {
-                ordered <- csv_1state[order(as.numeric(as.character(csv_1state$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack))) , ]
+                column <- 'Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack'
         }
         else if (outcome == "heart failure")
         {
-                ordered <- csv_1state[order(as.numeric(as.character(csv_1state$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure))) , ] 
+                column <- 'Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure' 
         }
         else if (outcome == "pneumonia")
         {
-                ordered <- csv_1state[order(as.numeric(as.character(csv_1state$Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia))) , ]
+                column <- 'Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia' 
         }
+        
+        
+        ordered <- csv_1state[order(as.numeric(as.character(csv_1state$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack))) , ]
+        ordered2 <- csv_1state[order(as.numeric(as.character(csv_1state[[column]]))) , ]
+        
         View(ordered)
+        View(ordered2)
+        if(is.numeric(num))
+        {
+                
+                        mortRate <- as.character(ordered[column], num)
+                        #print(mortRate)
+                        
+                        #sameMortRate <- ordered$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack == as.character(mortRate)
+                        #sameMortRate <- ordered[ordered$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack == as.character(mortRate),]
+                        #View(sameMortRate)
+        }
+                
+        #}
+        #else if (outcome == "heart failure")
+        #{
+        #        ordered <- csv_1state[order(as.numeric(as.character(csv_1state$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure))) , ] 
+        #        
+        #}
+        #else if (outcome == "pneumonia")
+        #
+        #        ordered <- csv_1state[order(as.numeric(as.character(csv_1state$Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia))) , ]
+        #}
+        
+        
+        #View(ordered)
         if(is.numeric(num))
         {
                 hospital <- as.character(ordered$Hospital.Name[num])
+        }
+        else if (num == "worst")
+        {
+                hospital <- as.character(ordered$Hospital.Name[length(ordered)])
+        }
+        else if (num == "best")
+        {
+                
         }
         hospital
         ## Check that state and outcome are valid
