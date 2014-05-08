bfe9c8ce855ef8dde8ca839bdf1de1c73b66f1f0
Wijnand Nuij <wijnandnuij@gmail.com>
2014-05-08 20:04:17 +0200

    start course project #1 expl analysis

---
+++ Exploratory_analysis/Course_project_1/graph.R
@@ -0,0 +1,7 @@
+
+createGraph <- function()
+{
+        require(data.table)
+        #data <- fread('/home/wijnand/household_power_consumption.txt', sep=";", na.strings='?')
+        print(nrow(data))
+}
