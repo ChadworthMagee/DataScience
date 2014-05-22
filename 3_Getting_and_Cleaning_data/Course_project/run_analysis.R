050166664a82e3006e93dee18aec94177af27270
Wijnand Nuij <wijnandnuij@gmail.com>
2014-05-22 22:33:36 +0200

    getting / cleaning data course project progress

--- 3_Getting_and_Cleaning_data/Course_project/run_analysis.R
+++ 3_Getting_and_Cleaning_data/Course_project/run_analysis.R
@@ -1,17 +1,39 @@
 
-runAnalysis <- function(sourceDir='/home/wijnand/Downloads/UCI HAR Dataset/test/Inertial Signals/')
+runAnalysis <- function(sourceDir='/home/wijnand/Downloads/UCI HAR Dataset/')
 {
-        require(data.table)
-        files <- list.files(sourceDir)
+        # load / read all relevant files 
+        activityLabels <- read.table(paste(sourceDir, "activity_labels.txt", sep=''), header=FALSE)
+        features <- read.table(paste(sourceDir, "features.txt", sep=''), header=FALSE)
+        trainX <- read.table(paste(sourceDir, "train/X_train.txt", sep=''), header=FALSE)
+        testX <- read.table(paste(sourceDir, "test/X_test.txt", sep=''), header=FALSE)
+        trainY <- read.table(paste(sourceDir, "train/y_train.txt", sep=''), header=FALSE)
+        testY <- read.table(paste(sourceDir, "test/y_test.txt", sep=''), header=FALSE)
+        trainTestSubject <- read.table(paste(sourceDir, "train/subject_train.txt", sep=''), header=FALSE)
+        testTestSubject <- read.table(paste(sourceDir, "test/subject_test.txt", sep=''), header=FALSE)
         
-        dataTable <- NULL
-        for(i in files)
+        # set column names
+        colnames(activityLabels) <- c('activityId','activityType')
+        colnames(trainX) <- features[,2]
+        colnames(trainY) <- "activityId"
+        colnames(trainTestSubject) <- "subjectId"
+        colnames(testX) <- features[,2]
+        colnames(testY) <- "activityId"
+        colnames(testTestSubject) <- "subjectId"
+        
+        
+        # step 1, merge training and test sets
+        trainData <- cbind(trainY, trainTestSubject, trainX)
+        testData <- cbind(testY, testTestSubject, testX)
+        allData <- rbind(trainData, testData)
+
+        # step 2, select the columns about mean / standard deviation (plus the activity / subject)
+        cols <- c(grep("mean|activity|subject|std", colnames(allData)))
+        filteredData <- allData[,cols]
+        
+        
+        # step 3, descriptive activity names
+        for(i in 1:length(filteredData[,1]))
         {
-                path <- paste(sourceDir, i, sep='')
-                x <- fread(path, sep='\n', sep2='-', header=FALSE)
-                View(x)
-                break
-                dataTable <- rbind(dataTable, x)
+                filteredData[i,1] <- as.character(activityLabels[filteredData[i,1],2])
         }
-        View(dataTable)
 }
